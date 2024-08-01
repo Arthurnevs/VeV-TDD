169e29ad4eff5c0ec5a08cdf6446472c60f677a0
arthurneves <dev6@beeteller.com>
2024-07-31 23:14:05 -0300

    feat: final tests to increase coverage

--- ProcessadorDeContas/src/test/java/ProcessadorDeContasTest.java
+++ ProcessadorDeContas/src/test/java/ProcessadorDeContasTest.java
@@ -16,10 +16,7 @@ public class ProcessadorDeContasTest {
 
     @Test
     public void testFaturaPagaComBoletosEmDia() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String dataString = "29/07/2024";
-        LocalDate data = LocalDate.parse(dataString, formatter);
-        Fatura fatura = new Fatura("1", data,666.69,"Laryssa");
+        Fatura fatura = new Fatura("1", LocalDate.now(),666.69,"Laryssa");
         ProcessadorDeBoletos processadorDeContas = new ProcessadorDeBoletos(fatura);
 
         ArrayList<Conta> contas = new ArrayList<>();
@@ -69,5 +66,81 @@ public class ProcessadorDeContasTest {
         processadorDeContas.processarBoletos(contas);
 
         assertEquals("PENDENTE", fatura.getStatus());
+    }
+
+    @Test
+    public void testFaturaPagaComBoletoAtrasado() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataString = "29/07/2024";
+        LocalDate data = LocalDate.parse(dataString, formatter);
+        Fatura fatura = new Fatura("1", data, 666.69, "Laryssa");
+        ProcessadorDeBoletos processadorDeContas = new ProcessadorDeBoletos(fatura);
+
+        ArrayList<Conta> contas = new ArrayList<>();
+        LocalDate dataBoletoAtrasado = data.minusDays(10);
+
+        contas.add(new Conta(dataBoletoAtrasado, 500.00, "1233", "BOLETO"));
+        contas.add(new Conta(dataBoletoAtrasado, 400.00, "24983", "BOLETO"));
+
+        processadorDeContas.processarBoletos(contas);
+
+        assertEquals("PAGA", fatura.getStatus());
+        assertEquals(500.00 * 1.10 + 400.00 * 1.10, fatura.getValorAbatido(), 0.01);
+    }
+
+    @Test
+    public void testFaturaNaoPagaComBoletoAbaixoDoLimite() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataString = "05/08/2024";
+        LocalDate data = LocalDate.parse(dataString, formatter);
+        Fatura fatura = new Fatura("1", data, 666.69, "Laryssa");
+        ProcessadorDeBoletos processadorDeContas = new ProcessadorDeBoletos(fatura);
+
+        ArrayList<Conta> contas = new ArrayList<>();
+        contas.add(new Conta(data, 0.00, "1233", "BOLETO")); // Valor abaixo do mínimo
+        contas.add(new Conta(data, 600.00, "24983", "BOLETO"));
+
+        processadorDeContas.processarBoletos(contas);
+
+        assertEquals("PENDENTE", fatura.getStatus());
+        assertEquals(600.00, fatura.getValorAbatido(), 0.01);
+    }
+
+    @Test
+    public void testFaturaNaoPagaComCartaoDeCreditoForaDoPrazo() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataString = "29/07/2024";
+        LocalDate data = LocalDate.parse(dataString, formatter);
+        Fatura fatura = new Fatura("1", data, 666.69, "Laryssa");
+        ProcessadorDeBoletos processadorDeContas = new ProcessadorDeBoletos(fatura);
+
+        ArrayList<Conta> contas = new ArrayList<>();
+        LocalDate dataCartaoForaPrazo = data.minusDays(10);
+
+        contas.add(new Conta(dataCartaoForaPrazo, 700.00, "29834", "CARTAO_CREDITO"));
+
+        processadorDeContas.processarBoletos(contas);
+
+        assertEquals("PENDENTE", fatura.getStatus());
+        assertEquals(0.00, fatura.getValorAbatido(), 0.01);
+    }
+
+    @Test
+    public void testFaturaPagaComTransferenciaBancariaDentroDoPrazo() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataString = "29/07/2024";
+        LocalDate data = LocalDate.parse(dataString, formatter);
+        Fatura fatura = new Fatura("1", data, 666.69, "Laryssa");
+        ProcessadorDeBoletos processadorDeContas = new ProcessadorDeBoletos(fatura);
+
+        ArrayList<Conta> contas = new ArrayList<>();
+        LocalDate dataTransferenciaDentroPrazo = data.minusDays(5);
+
+        contas.add(new Conta(dataTransferenciaDentroPrazo, 700.00, "29834", "TRANSFERENCIA_BANCARIA"));
+
+        processadorDeContas.processarBoletos(contas);
+
+        assertEquals("PAGA", fatura.getStatus());
+        assertEquals(700.00, fatura.getValorAbatido(), 0.01);
     }
 }
